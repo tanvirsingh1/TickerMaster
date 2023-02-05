6219db79a1969568c8f4fb31542cdb67380a8251
Zander M <zander@zandercraft.ca>
2023-02-05 00:14:44 -0500

    Add class docstring to TicketingConfig as required by C0115

--- ticket_sprint/settings.py
+++ ticket_sprint/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ticketing.apps.TicketSprintConfig',
+    'ticketing.apps.TicketingConfig',
 ]
 
 MIDDLEWARE = [
--- ticketing/apps.py
+++ ticketing/apps.py
@@ -4,7 +4,10 @@ apps.py - Responsible for storing the application's metadata
 from django.apps import AppConfig
 
 
-class TicketSprintConfig(AppConfig):
+class TicketingConfig(AppConfig):
+    """
+    Contains metadata for the Ticketing application.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ticketing'
     verbose_name = 'Ticketing'
