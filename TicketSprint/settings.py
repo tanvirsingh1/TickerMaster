12e32bfd7bf1b3a69ca50b42afd5e493542ab5b1
Zander M <zander@zandercraft.ca>
2023-02-04 23:40:31 -0500

    Fix django URL conf

--- TicketSprint/settings.py
+++ TicketSprint/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'BTP405_Project1.urls'
+ROOT_URLCONF = 'TicketSprint.urls'
 
 TEMPLATES = [
     {
