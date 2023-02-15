578f3e081216f3c816a12cc9d9e5270e5f53a4bb
Zander M <zander@zandercraft.ca>
2023-02-14 22:41:11 -0500

    Add missing docstring to VenueManagementConfig class

--- venue_management/apps.py
+++ venue_management/apps.py
@@ -6,5 +6,8 @@ from django.apps import AppConfig
 
 
 class VenueManagementConfig(AppConfig):
+    """
+    Contains metadata for the Venue Management application.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'venue_management'
