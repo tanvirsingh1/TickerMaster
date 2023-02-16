2174b2176c857156c224673e2fa1a0e3242c7ee5
Zander M <zander@zandercraft.ca>
2023-02-16 01:30:21 -0500

    Fix pylint_ignore monkeypatch by using expand_modules properly.

--- pylint_ignore.py
+++ pylint_ignore.py
@@ -3,14 +3,14 @@ Pylint Path ignore monkey-patch by https://github.com/jw-lilly
 Source: https://github.com/PyCQA/pylint/issues/2686#issuecomment-621927895
 """
 
-from pylint.utils import utils
+from pylint.lint import expand_modules
 
 
 class PylintIgnorePaths:
     def __init__(self, *paths):
         self.paths = paths
-        self.original_expand_modules = utils.expand_modules
-        utils.expand_modules = self.patched_expand
+        self.original_expand_modules = expand_modules.expand_modules
+        expand_modules.expand_modules = self.patched_expand
 
     def patched_expand(self, *args, **kwargs):
         result, errors = self.original_expand_modules(*args, **kwargs)
@@ -18,7 +18,6 @@ class PylintIgnorePaths:
         def keep_item(item):
             if any(1 for path in self.paths if item['path'].startswith(path)):
                 return False
-
             return True
 
         result = list(filter(keep_item, result))
