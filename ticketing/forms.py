5132ccd5c8b3cc0d3c592c077bec2984c4b8bf2e
Zander M <zander@zandercraft.ca>
2023-02-20 15:27:53 -0500

    C0114: Add missing module docstring

--- ticketing/forms.py
+++ ticketing/forms.py
@@ -1,9 +1,16 @@
+"""
+forms.py - Responsible for defining forms for the Ticketing application
+"""
+
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
 class RegisterForm(UserCreationForm):
+    """
+    The registration form for Eventgoers. Creates an account when valid info is given.
+    """
     username = forms.CharField(label='Username', widget=forms.TextInput(
         attrs={'class': 'form-control', 'placeholder': 'Enter your username'}))
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput(
@@ -12,5 +19,8 @@ class RegisterForm(UserCreationForm):
         attrs={'class': 'form-control', 'placeholder': 'Confirm your password'}))
 
     class Meta:
+        """
+        Defines the form's metadata
+        """
         model = User
         fields = ['username', 'password1', 'password2']
