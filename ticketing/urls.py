dfeefb359ba19e16329dda1ebf9ba691721402aa
Zander M <zander@zandercraft.ca>
2023-03-02 14:34:45 -0500

    C0303: Remove trailing whitespace

--- ticketing/urls.py
+++ ticketing/urls.py
@@ -3,7 +3,7 @@ urls.py - Responsible for defining URL routing for the Ticketing application
 """
 
 from django.urls import path
-from . import views 
+from . import views
 
 urlpatterns = [
     path('login', views.login_window, name="Login Window" ),
