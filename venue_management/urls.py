c6257964fc0007398726e133a289545566f5082a
Zander M <zander@zandercraft.ca>
2023-03-02 14:41:37 -0500

    W0611: Remove Unused Import and change URL back to venue/

--- venue_management/urls.py
+++ venue_management/urls.py
@@ -2,11 +2,11 @@
 urls.py - Responsible for defining URL routing for the Venue Management application
 """
 
-from django.urls import path, include
+from django.urls import path
 from .views import generate_promo_code
 from . import views
 
 urlpatterns = [
-    path('/ven', views.index, name='index'),
+    path('venue/', views.index, name='index'),
     path('generate-promo-code/', generate_promo_code, name='generate_promo_code')
 ]
