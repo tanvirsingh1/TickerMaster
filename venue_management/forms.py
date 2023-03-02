4341718687edaf2abb23caa4132658c01101d157
Zander M <zander@zandercraft.ca>
2023-03-02 14:33:54 -0500

    C0114: Add missing module docstring C0115: Add missing class docstring

--- venue_management/forms.py
+++ venue_management/forms.py
@@ -1,4 +1,13 @@
+"""
+forms.py - Responsible for defining forms for the Venue Management application
+"""
+
+# Imports
 from django import forms
 
+
 class PromoCodeForm(forms.Form):
+    """
+    TODO: What is this?
+    """
     name = forms.CharField(label='Your Name')
