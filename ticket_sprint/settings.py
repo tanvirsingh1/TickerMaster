8c367a4165e84927e716212eabe53e1e543e5d26
Meetsimar <meetsimark@gmail.com>
2023-03-04 00:37:07 -0500

    my work

--- ticket_sprint/settings.py
+++ ticket_sprint/settings.py
@@ -128,6 +128,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+MEDIA_ROOT =os.path.join(BASE_DIR,'venue_management/static/images')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- venue_management/admin.py
+++ venue_management/admin.py
@@ -5,7 +5,10 @@ admin.py - Responsible for managing custom logic for the admin interface
 from django.contrib import admin
 from . import models
 
+
 # Register models with the Admin site.
 admin.site.register(models.VenueManager)
 admin.site.register(models.Venue)
 admin.site.register(models.Concert)
+admin.site.register(models.Location)
+admin.site.register(models.SeatType)
