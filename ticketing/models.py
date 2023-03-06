cec646732397e8f18a6f5aac39a201aaf993f12c
Zander M <zander@zandercraft.ca>
2023-03-05 21:43:07 -0500

    C0305, W0611: Remove trailing newline and unused import

--- ticketing/models.py
+++ ticketing/models.py
@@ -3,7 +3,6 @@ models.py - Contains all data models for the application
 """
 
 from django.db import models
-from django.core import validators
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth import get_user_model
 from .manager import UserManager
@@ -91,4 +90,3 @@ class SupportTicket(models.Model):
         Returns the subject of the support ticket as a string.
         """
         return str(self.subject)
-
