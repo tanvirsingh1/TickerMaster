d12b594ae27e6c614a020d8336e64f73b6afc28e
Zander <28664144+ZandercraftGames@users.noreply.github.com>
2023-03-07 00:21:44 -0500

    Don't comment out model stuff. Debug on your own end...
    If having issues with models, don't break them. Wipe DB and migrations and start it over

--- venue_management/models.py
+++ venue_management/models.py
@@ -199,8 +199,7 @@ class Venue(models.Model):
     Describes a Venue
     """
     name = models.CharField(max_length=60, verbose_name="Name")
-    #have error because of this
-    #description = models.CharField(max_length=255, verbose_name="Description", default=None)
+    description = models.CharField(max_length=255, verbose_name="Description", default=None)
     image = models.ImageField(max_length=255, verbose_name="Image")
     website = models.URLField(max_length=255, verbose_name="Website", null=False)
     location = models.ForeignKey(Location, on_delete=models.PROTECT, verbose_name="Location")
